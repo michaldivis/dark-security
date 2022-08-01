2c5f01c9176a6648451156cfa2fae639581bdad8
Michal Diviš <A408674@dekra.org>
2022-08-01 11:56:21 +0200

    added build script

--- build/Build.cs
+++ build/Build.cs
@@ -1,44 +1,105 @@
-using System;
-using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
-using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
+using System.Linq;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
+    public static int Main() => Execute<Build>(x => x.PushNuget);
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    private const string CoreProjectName = "DarkSecurity";
+    private const string TestProjectName = "DarkSecurityTests";
 
-    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+    [Solution] readonly Solution Solution;
+
+    AbsolutePath SourceDirectory => RootDirectory / "src";
+    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+
+    readonly Configuration Configuration = Configuration.Release;
+
+    [Parameter] readonly string NugetApiUrl = "https://api.nuget.org/v3/index.json";
+    [Parameter] readonly string NugetApiKey;
 
     Target Clean => _ => _
-        .Before(Restore)
+        .Before(RestoreCore)
         .Executes(() =>
         {
+            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
         });
 
-    Target Restore => _ => _
+    Target RestoreCore => _ => _
         .Executes(() =>
         {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution.GetProject(CoreProjectName)));
         });
 
-    Target Compile => _ => _
-        .DependsOn(Restore)
+    Target CompileCore => _ => _
+        .DependsOn(RestoreCore)
         .Executes(() =>
         {
+            DotNetBuild(s => s
+                .SetProjectFile(Solution.GetProject(CoreProjectName))
+                .SetConfiguration(Configuration)
+                .EnableNoRestore());
+        });
+
+    Target CompileTests => _ => _
+        .DependsOn(CompileCore)
+        .Executes(() =>
+        {
+            DotNetBuild(s => s
+                .SetProjectFile(Solution.GetProject(TestProjectName))
+                .SetConfiguration(Configuration)
+                .EnableNoRestore());
+        });
+
+    Target RunTests => _ => _
+        .DependsOn(CompileTests)
+        .Executes(() =>
+        {
+            DotNetTest(s => s
+                .SetProjectFile(Solution.GetProject(TestProjectName))
+                .SetConfiguration(Configuration)
+                .EnableNoRestore()
+                .EnableNoBuild());
+        });
+
+
+    Target Pack => _ => _
+        .DependsOn(RunTests)
+        .Executes(() =>
+        {
+            DotNetPack(s => s
+              .SetProject(Solution.GetProject(CoreProjectName))
+              .SetConfiguration(Configuration)
+              .EnableNoBuild()
+              .EnableNoRestore()
+              .SetOutputDirectory(ArtifactsDirectory));
+        });
+
+    Target PushNuget => _ => _
+        .DependsOn(Pack)
+        .Requires(() => NugetApiUrl)
+        .Requires(() => NugetApiKey)
+        .Executes(() =>
+        {
+            GlobFiles(ArtifactsDirectory, "*.nupkg")
+               .Where(x => !string.IsNullOrEmpty(x) && !x.EndsWith("symbols.nupkg"))
+               .ForEach(x =>
+               {
+                   DotNetNuGetPush(s => s
+                       .SetTargetPath(x)
+                       .SetSource(NugetApiUrl)
+                       .SetApiKey(NugetApiKey)
+                   );
+               });
         });
 
 }
